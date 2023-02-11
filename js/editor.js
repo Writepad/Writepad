df797267dfab99a08e86a17d30441130d1dbbd6b
Cyril John Magayaga <cjmagayaga957@gmail.com>
2023-02-11 12:29:07 +0800

    first initial release

---
+++ js/editor.js
@@ -0,0 +1,25 @@
+const textArea = document.querySelector("textarea");
+const saveButton = document.querySelector("#save-button");
+const saveButton1 = document.querySelector("#save-button1");
+const reloadButton = document.querySelector("#reload-button");
+
+reloadButton.addEventListener("click", () => {
+  window.location.reload();
+})
+saveButton.addEventListener("click", () => {
+  const text = textArea.value;
+  const blob = new Blob([text], { type: "text/plain" });
+  const link = document.createElement("a");
+  link.download = "index.txt";
+  link.href = URL.createObjectURL(blob);
+  link.click();
+});
+
+saveButton1.addEventListener("click", () => {
+  const text = textArea.value;
+  const blob = new Blob([text], { type: "text/html" });
+  const link = document.createElement("a");
+  link.download = "index.html";
+  link.href = URL.createObjectURL(blob)
+  link.click();
+});
