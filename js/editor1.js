e1825ad9c452d7a1a866917ceb7250353b6f0ed1
Cyril John Magayaga <cjmagayaga957@gmail.com>
2023-02-14 15:01:28 +0800

    Update release

--- js/editor1.js
+++ js/editor1.js
@@ -8,11 +8,9 @@ function showTime() {
 
 showTime();
 
-// Date
 function updateDate() {
   let today = new Date();
 
-  // return number
   let dayName = today.getDay(),
     dayNum = today.getDate(),
     month = today.getMonth(),
@@ -41,9 +39,7 @@ function updateDate() {
     "Friday",
     "Saturday",
   ];
-  // value -> ID of the html element
   const IDCollection = ["day", "daynum", "month", "year"];
-  // return value array with number as a index
   const val = [dayWeek[dayName], dayNum, months[month], year];
   for (let i = 0; i < IDCollection.length; i++) {
     document.getElementById(IDCollection[i]).firstChild.nodeValue = val[i];
--- js/editor2.js
+++ js/editor2.js
@@ -2,7 +2,6 @@ function dispFile(contents) {
   document.getElementById('contents').innerHTML=contents
 }
 function clickElem(elem) {
-	// Thx user1601638 on Stack Overflow (6/6/2018 - https://stackoverflow.com/questions/13405129/javascript-create-and-save-file )
 	var eventMouse = document.createEvent("MouseEvents")
 	eventMouse.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
 	elem.dispatchEvent(eventMouse)
