1a2cad2fa77269fd9c417ea6a588e6fc68fb4b35
Cyril John Magayaga <cjmagayaga957@gmail.com>
2023-02-16 21:29:41 +0800

    Update release

--- js/editor1.js
+++ js/editor1.js
@@ -1,5 +1,5 @@
 const displayTime = document.querySelector(".display-time");
-// Time
+
 function showTime() {
   let time = new Date();
   displayTime.innerText = time.toLocaleTimeString("en-US", { hour12: false });
